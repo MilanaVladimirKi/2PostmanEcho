758f1b87e8f49b581e4f51486c50064f8937b529
Milana Kichutkina <MilanaVladimir@mail.ru>
2024-08-25 23:12:02 +0500

    GOOD test

---
+++ src/test/java/ru/topacademy/postmanTest.java
@@ -0,0 +1,22 @@
+package ru.topacademy;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class postmanTest {
+
+    @Test
+    public void test() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
